bdd99b3f0c2cd993b3ebe87d12570930231b964a
Amanda Rodrigues <amanda.admi@hotmail.com>
2022-04-26 22:31:02 -0300

    potencia

--- potencia.c
+++ potencia.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
 
 int main()
 {
   int numero,potencia;
 
-
   scanf("%d",&numero);
- potencia = pow(numero,3);
+  potencia = pow(numero,3);
 
-printf("%d\n", potencia);
-
+  printf("%d \n", potencia);
 
   return 0;
-
 }
