27e9d2f6f2f21961d243b4e5eec9b72cbcba94f9
Amanda Rodrigues <amanda.admi@hotmail.com>
2022-04-26 21:09:23 -0300

    potencia

---
+++ potencia.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+  int numero,potencia;
+
+
+  scanf("%d",&numero);
+ potencia = pow(numero,3);
+
+printf("%d\n", potencia);
+
+
+  return 0;
+
+}
