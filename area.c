2b05f3d38bc535bc16d66f60a0117e43da3560d1
Amanda Rodrigues <amanda.admi@hotmail.com>
2022-04-27 21:39:29 -0300

    area

--- area.c
+++ area.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main () {
-  float altura, base, areatriangulo, arearetangulo;
-
+  float altura, base, areatriangulo, arearetangulo, areaquadrado, raio, areacirculo, pi=3.14;
   
   printf("Declare a base do triangulo: ");
   scanf("%f", &base);
@@ -10,20 +9,32 @@ int main () {
   areatriangulo = (base*altura)/2;
   printf( "Area do triangulo é igual a: %.2f. \n", areatriangulo);
 
-
   printf("Declare a base do retangulo: ");
   scanf("%f", &base);
   printf("Declare a altura do retangulo: ");
   scanf("%f", &altura);
   arearetangulo = base*altura;
-  printf( "Area do triangulo é igual a: %f\n", arearetangulo);
+  printf( "Area do triangulo é igual a: %.2f. \n", arearetangulo);
+
+
+  printf("Declare a base do quadrado: ");
+  scanf("%f", &base);
+  printf("Declare a altura do quadrado: ");
+  scanf("%f", &altura);
+  areaquadrado = base*altura;
+  printf( "Area do quadrado é igual a: %.2f. \n", areaquadrado);
+
+
+  printf("Declare o raio do  circulo: ");
+  scanf("%f", &raio);  
+  areacirculo = pi*raio*raio;
+  printf( "Area do quadrado é igual a: %.2f. \n", areacirculo);
+
 
   return 0;
-
-  
 
 }
 
 
 
-//Calcular a área de um retângulo, quadrado, triângulo e o círculo.
+//Calcular a área do círculo.
