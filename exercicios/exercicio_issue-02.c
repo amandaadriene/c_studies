313333ff4209d0c5669d8904315f722d7f29f26f
Amanda Rodrigues <amanda.admi@hotmail.com>
2022-05-04 22:24:39 -0300

    Refatoração com funções - exercicio 2

--- exercicios/exercicio_issue-02.c
+++ exercicios/exercicio_issue-02.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 
-int main()
-{
-  int idade;
+int ler_idade(void);
+void verifica_maior_idade (int idade);
 
-  printf("Digite sua idade:");
-  scanf("%d", &idade);
-
-  if(idade >= 65)
-  {
-    printf("Você pode aposentar!\n");
-  }
-  else 
-  {
-    printf("Você não pode aposentar!\n");
-  }
+int main(void) {
+  int valor_idade;
   
+  valor_idade = ler_idade();
+  verifica_maior_idade(valor_idade);
+
   return 0;
 }
+
+int ler_idade (void) {
+  int idade;
+
+  printf("Digite sua idade: ");
+  scanf("%d", &idade);
+
+  return idade;
+}
+
+void verifica_maior_idade (int idade) {
+  if(idade >= 18)
+  {
+    printf("Você é maior de idade!\n");
+  }
+  else
+  {
+    printf("Você é menor de idade!\n");
+  }
+} 
+
+
